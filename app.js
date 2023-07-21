8a16d61555e64b273384e800d15d4c07f2d26bc8
surazkarn <surazkarn8848@gmail.com>
2023-07-21 15:19:25 +0530

    routes setup

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
-
+const trainsRoutes = require('./routes/trainRoutes');
 
 const app = express();
 const port = 3000; 
 
 app.use(express.json());
 
-
+app.use('/trains', trainsRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
